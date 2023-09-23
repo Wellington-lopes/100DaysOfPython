00d79de475523092e10df5be6703b01fe3bb332b
Wellington Lopes <wellington.slopes1991@gmail.com>
2023-09-23 00:40:36 -0300

    Add files via upload

---
+++ fizzbuzz/main.py
@@ -0,0 +1,10 @@
+fizz = 0
+for n in range(1, 101):
+    if (n % 3 == 0) and (n % 5 == 0):
+        print("FizzBuzz")
+    elif n % 5 == 0:
+        print("Buzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    else:
+        print(n)
